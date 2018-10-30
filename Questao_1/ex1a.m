c6eda795d6c5c91acf1192a1c4f36adacead68f5
Rodrigo Figueiredo <rodrigofigueiredo@tecnico.ulisboa.pt>
2018-10-30 10:26:16 +0000

    Questao 1 (muito incompleto)

---
+++ Questao_1/ex1a.m
@@ -0,0 +1,11 @@
+function y = ex1a
+%UNTITLED9 Summary of this function goes here
+%   Detailed explanation goes here
+Rodrigo = 190185;
+Rafael = 190171;
+Joao = 190119;
+Vasco = 190205;
+
+y = Rodrigo + Rafael + Joao + Vasco;
+end
+
